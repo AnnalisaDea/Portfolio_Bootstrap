535007a0674c7cd40b75e5f1d2ff80a39d28b70f
annalisa <annalisa.deanesi@gmail.com>
2024-03-01 10:23:17 +0100

    Topnav scrollUp JS

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
+let prevScrollpos = window.scrollY;
+const topnav = document.getElementById("topnav");
+
+
 window.addEventListener('load', () => {
     const loadingScreen = document.getElementById("loading");
     setTimeout(function () {
         loadingScreen.style.display = 'none';
     }, 2000); // Ritardo di 2000 millisecondi (2 second1)
 })
+
+window.addEventListener('scroll', () => {
+    let currentScrollPos = window.scrollY;
+
+
+    if (prevScrollpos < currentScrollPos) {
+        // Scroll down
+        topnav.classList.add("hidden");
+    } else {
+        // Scroll up
+        topnav.classList.remove("hidden");
+
+    }
+    prevScrollpos = currentScrollPos;
+});
 
 
