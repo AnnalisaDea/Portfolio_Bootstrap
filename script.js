e6444269a2c574248667a5ede9e7e41d6ae0af06
annalisa <annalisa.deanesi@gmail.com>
2024-03-01 10:16:53 +0100

    Loading JS

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', () => {
+    const loadingScreen = document.getElementById("loading");
+    setTimeout(function () {
+        loadingScreen.style.display = 'none';
+    }, 2000); // Ritardo di 2000 millisecondi (2 second1)
+})
+
+
