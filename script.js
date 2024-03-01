8d2479b95bf44a2201879a01b153fdfaf949c8a7
annalisa <annalisa.deanesi@gmail.com>
2024-03-01 11:09:29 +0100

    Animazione work

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 let prevScrollpos = window.scrollY;
 const topnav = document.getElementById("topnav");
 
+function anim() {
+    let sections = gsap.utils.toArray(".panel");
+
+
+    gsap.to(sections, {
+        xPercent: -100 * (sections.length - 1),
+        ease: "none",
+        scrollTrigger: {
+            trigger: "#works",
+            start: "top top",
+            pin: true,
+            scrub: 1,
+            snap: {
+                snapTo: [0, 0.5, 1],
+                directional: false
+            },
+            end: () => "+=" + document.querySelector("#works").offsetWidth
+        }
+    });
+}
+
 
 window.addEventListener('load', () => {
     const loadingScreen = document.getElementById("loading");
@@ -24,4 +45,4 @@ window.addEventListener('scroll', () => {
     prevScrollpos = currentScrollPos;
 });
 
-
+document.addEventListener("DOMContentLoaded", anim)
